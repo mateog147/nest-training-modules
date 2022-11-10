9efc67a2d4c62af672afeec364dce9e53805e34a
mateosofka <mateo.gutierrez@sofka.com.co>
2022-11-10 08:51:48 -0500

    fix: interceptor bug

--- src/interceptors/transform.interceptor.ts
+++ src/interceptors/transform.interceptor.ts
@@ -6,20 +6,24 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { User } from 'src/users/interfaces/user';
 
 export interface Response<T> {
   data: T;
 }
 
 @Injectable()
-export class TransformInterceptor<T>
-  implements NestInterceptor<T, Response<T>>
-{
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<Response<T>> {
-    console.log('context :>> ', context);
-    return next.handle().pipe(map((data) => ({ ...data, lastName: '' })));
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (data.lenght > 1) {
+          return data.map((user: User) =>
+            user.lastName == undefined ? { ...user, lastName: null } : user,
+          );
+        }
+        return data.lastname === undefined ? { ...data, lastname: null } : data;
+      }),
+    );
   }
 }
